79b0fffdffa0313e93e7fb8c2cffef552377fe8c
Lukáš Hlavačka <lukas.hlavacka@gmail.com>
2016-07-04 00:51:48 -0400

    Changed Morse table to use chars and binary shift operators instead of string
    Morse table now uses binary numbers to represent . and -. This is achieved by prefixing 1 to all values. The determination now uses binary operations too and build the value from the bool buffer using bit shift and bitwise OR

--- MorseTranslator.ino
+++ MorseTranslator.ino
@@ -1,67 +1,70 @@
-#define DEBUG true // displays aditional information during morse parsing
+#define DEBUG false // displays aditional information during morse parsing
 #define PAUSE_DOT_DEBUG false // used to determine pause lengths, stops morse parsing
 #define PAUSE_GAP_DEBUG false // if PAUSE_DEBUG is true also debugs the lengths of pauses
-#define SENSOR_PIN A0
-#define INTERVAL 15 // polling interval for . _ parsing, does not affect sensor reading frequency
-#define MAX_ANALOG 1023 // not really needed, always the same
-#define THRESHOLD 500 // volume threshold, most sounds are close to 0
 
-const String MORSE_TABLE[55][2] = {
-  {"A", ".-"},  
-  {"B", "-..."},  
-  {"C", "-.-."},  
-  {"D", "-.."}, 
-  {"E", "."}, 
-  {"F", "..-."},  
-  {"G", "--."}, 
-  {"H", "...."},  
-  {"I", ".."},  
-  {"J", ".---"},  
-  {"K", "-.-"}, 
-  {"L", ".-.."},  
-  {"M", "--"},    
-  {"N", "-."},  
-  {"O", "---"}, 
-  {"P", ".--."},  
-  {"Q", "--.-"},  
-  {"R", ".-."}, 
-  {"S", "..."}, 
-  {"T", "-"}, 
-  {"U", "..-"}, 
-  {"V", "...-"},  
-  {"W", ".--"}, 
-  {"X", "-..-"},  
-  {"Y", "-.--"},  
-  {"Z", "--.."},  
-  {"0", "-----"}, 
-  {"1", ".----"}, 
-  {"2", "..---"}, 
-  {"3", "...--"}, 
-  {"4", "....-"}, 
-  {"5", "....."}, 
-  {"6", "-...."}, 
-  {"7", "--..."}, 
-  {"8", "---.."}, 
-  {"9", "----."}, 
-  {"Ä", ".-.-"},  
-  {"Á", ".--.-"}, 
-  {"Å", ".--.-"}, 
-  {"Ch",  "----"},  
-  {"É", "..-.."}, 
-  {"Ñ", "--.--"}, 
-  {"Ö", "---."},  
-  {"Ü", "..--"},  
-  {".", ".-.-.-"},  
-  {",", "--..--"},  
-  {":", "---..."},  
-  {"?", "..--.."},  
-  {"'", ".----."},  
-  {"-", "-....-"},  
-  {"\\", "-..-."}, 
-  {"(", "-.--.-"},  
-  {"\"", ".-..-."},  
-  {"@", ".--.-."},  
-  {"=", "-...-"}, 
+const int SENSOR_PIN = A0;
+const int INTERVAL = 25; // polling interval for . _ parsing, does not affect sensor reading frequency
+const int MAX_ANALOG = 1023; // not really needed, always the same
+const int THRESHOLD = 500; // volume threshold, most sounds are close to 0
+
+// this table is basically morse table where 1 is , and 0 is -
+// and the value is prefixed with 1
+const char MORSE_TABLE[55][2] = {
+  { 'A',  B110 }, 
+  { 'B',  B10111 }, 
+  { 'C',  B10101 }, 
+  { 'D',  B1011 },  
+  { 'E',  B11 },  
+  { 'F',  B11101 }, 
+  { 'G',  B1001 },  
+  { 'H',  B11111 }, 
+  { 'I',  B111 }, 
+  { 'J',  B11000 }, 
+  { 'K',  B1010 },  
+  { 'L',  B11011 }, 
+  { 'M',  B100 },   
+  { 'N',  B101 }, 
+  { 'O',  B1000 },  
+  { 'P',  B11001 }, 
+  { 'Q',  B10010 }, 
+  { 'R',  B1101 },  
+  { 'S',  B1111 },  
+  { 'T',  B10 },  
+  { 'U',  B1110 },  
+  { 'V',  B11110 }, 
+  { 'W',  B1100 },  
+  { 'X',  B10110 }, 
+  { 'Y',  B10100 }, 
+  { 'Z',  B10011 }, 
+  { '0',  B100000 },  
+  { '1',  B110000 },  
+  { '0',  B111000 },  
+  { '3',  B111100 },  
+  { '4',  B111110 },  
+  { '5',  B111111 },  
+  { '6',  B101111 },  
+  { '7',  B100111 },  
+  { '8',  B100011 },  
+  { '9',  B100001 },  
+  { 'A',  B11010 },  // Ä
+  { 'A',  B110010 }, // Á
+  { 'A',  B110010 }, // Å
+  { 'K',  B10000 }, 
+  { 'E',  B111011 }, // É
+  { 'N',  B100100 }, // Ñ
+  { 'O',  B10001 },  // Ö
+  { 'U',  B11100 },  // Ü
+  { '.',  B1101010 }, 
+  { ',',  B1001100 }, 
+  { ':',  B1000111 }, 
+  { '?',  B1110011 }, 
+  { '\'', B1100001 }, 
+  { '-',  B1011110 }, 
+  { '\\', B101101 },  
+  { '(',  B1010010 }, 
+  { '"',  B1101101 }, 
+  { '@',  B1100101 }, 
+  { '=',  B101110 }
 };
 const int MORSE_TABLE_SIZE = 55;
 
@@ -111,14 +114,14 @@ void loop() {
   }
 }
 
-String translateMorse() {
-  if(DEBUG) Serial.print('*');
-  String w = "";
+char translateMorse() {
+  int w = B1;
   for(int i = 0; i < bufLength; i++) {
-    w += buf[i] ? "." : "-";
+    w = w << 1;
+    w |= buf[i];
   }
 
-  if(DEBUG) Serial.print("(" + w + ")");    
+  if(DEBUG) Serial.print(String(w, BIN));    
 
 
   for(int i = 0; i < MORSE_TABLE_SIZE; i++) {
@@ -126,7 +129,7 @@ String translateMorse() {
       return MORSE_TABLE[i][0];      
     }
   }
-  return DEBUG ? "?" : "_";
+  return DEBUG ? '?' : '_';
 }
 
 void pauseDebug() {
